06c8afecd385d6d50fe16a90666644426f4636c9
得邪 <2281741446@qq.com>
2022-06-06 22:24:39 +0800

    axios于后端通信测试

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <p style="color: red">扎不多的了</p>
+    <p style="color: orange">扎不多的了</p>
+    <p style="color: slateblue">扎不多的了</p>
+    <p style="color: green">扎不多的了</p>
+    <p style="color: blue">扎不多的了</p>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import HelloWorld from "./components/HelloWorld.vue";
+import axios from "axios";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  created() {
+    axios.get("http://localhost:3000/login").then((res) => {
+      console.log(res);
+    });
+  },
+};
 </script>
 
 <style>
